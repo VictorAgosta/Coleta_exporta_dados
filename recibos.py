15142be48398a9a3f8ac0a050f5bf6b467248a6d
VictorAgosta <107890899+VictorAgosta@users.noreply.github.com>
2022-08-23 11:24:37 -0300

    Update recibos.py

--- recibos.py
+++ recibos.py
@@ -12,12 +12,14 @@ with open('empresa_mae.txt') as f_1:
 with open('chave.txt') as f_2:
     chave_ed = f_2.read()
 
-data_inicio = '01/01/2022'
-data_fim = '07/07/2022'
+d_inicio = '01/01/2022'
+d_fim = '25/07/2022'
+data_inicio = d_inicio.replace(r'/', '-')
+data_fim = d_fim.replace(r'/', '-')
 
 # definição dos caminhos para leitura e criação dos arquivos excel
-leitura = r'C:\Users\desenvolvimento02\Desktop\esocial_teste.xlsx'
-criacao = rf'C:\Users\desenvolvimento02\Desktop\Recibos eSocial\Recibos_eSocial_'
+leitura = r'X:\e-Social\dados\empresas_esocial.xlsx'
+criacao = r'X:\e-Social\Recibos eSocial\Recibos_eSocial'
 
 # leitura do arquivo excel com nome e cod das empresas
 empresas_df = pd.read_excel(leitura)
@@ -27,34 +29,42 @@ evento = []
 
 # condição para não inserir na lista caso só tenha o cabeçalho
 condicao = 'EVENTO;DATAGERACAO;UNIDADE;NOMEUNIDADE;CODIGOGED;CODIGOARQUIVOGED;' \
-            'NOMEARQUIVO;FUNCIONARIO;NOMEFUNCIONARIO;STATUSEVENTO;CODIGOLOTE;NRRECIBO;' \
-            'IDARQUIVO;ERRO;SEQUENCIAL;AMBIENTEPRODUCAO;CODIGOERROESOCIAL;DATAINICIOCONDICAO;CARGAINICIAL'
+           'NOMEARQUIVO;FUNCIONARIO;NOMEFUNCIONARIO;STATUSEVENTO;CODIGOLOTE;NRRECIBO;' \
+           'IDARQUIVO;ERRO;SEQUENCIAL;AMBIENTEPRODUCAO;CODIGOERROESOCIAL;DATAINICIOCONDICAO;CARGAINICIAL'
 
 # estrutura de repetição para os codigos e nomes das empresas do arquivo xlsx
 for codigo in empresas_df['cod']:
     indice = empresas_df[empresas_df['cod'] == codigo].index
     nome = empresas_df['nome'].values[indice]
 
-    navegador = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--incognito")
+
+    navegador = webdriver.Chrome(options=options)
 
 # navegador receberá a string do exporta dados do SOC referente aos eventos do eSocial
     navegador.get("https://ws1.soc.com.br/WebSoc/exportadados?parametro={"
                   + f"'empresa':'{empresa_mae}','codigo':'141273',"
-                  f"'chave':'{chave_ed}','tipoSaida':'txt','empresaTrabalho':{str(codigo)}'"
-                  f"','dataInicio':'{data_inicio}','dataFim':'{data_fim}',"
+                  f"'chave':'{chave_ed}','tipoSaida':'txt','empresaTrabalho':'{codigo}',"
+                  f"'dataInicio':'{d_inicio}','dataFim':'{d_fim}',"
                   "'status':'2','layout':'0','unidade':'0','ambiente':'1','cabecalho':'1'}")
-    csv = WebDriverWait(navegador, 50).until(ec.element_to_be_clickable((By.XPATH, '/html/body/pre'))).text
+    csv = WebDriverWait(navegador, 300).until(ec.element_to_be_clickable((By.XPATH, '/html/body/pre'))).text
+
+    if csv == condicao:
+        continue
 
 # tratativa do texto copiado do exporta dados gerando um xlsx para cada empresa
     plan = csv.split("\n")
     for linha in plan:
-        if linha == condicao:
-            continue
         coluna = linha.split(';')
         evento.append(coluna)
     navegador.close()
 
-    inconsistencias_df = pd.DataFrame(evento)
+    inconsistencias_df = pd.DataFrame(evento, columns=['EVENTO', 'DATAGERACAO', 'UNIDADE', 'NOMEUNIDADE', 'CODIGOGED',
+                                                       'CODIGOARQUIVOGED', 'NOMEARQUIVO', 'FUNCIONARIO',
+                                                       'NOMEFUNCIONARIO', 'STATUSEVENTO', 'CODIGOLOTE', 'NRRECIBO',
+                                                       'IDARQUIVO', 'ERRO', 'SEQUENCIAL', 'AMBIENTEPRODUCAO',
+                                                       'CODIGOERROESOCIAL', 'DATAINICIOCONDICAO', 'CARGAINICIAL'])
 
     empresa = str(nome)
 
@@ -63,12 +73,12 @@ for codigo in empresas_df['cod']:
 
     nome_empresa = empresa[2:fim]
 
-    inconsistencias_df.to_excel(rf'{criacao}{nome_empresa}.xlsx',
+    inconsistencias_df.to_excel(f'{criacao}_{codigo}_{nome_empresa}_{data_inicio}_a_{data_fim}.xlsx',
                                 index=False)
     evento = []
 
 # Alterando a parte visual das planilhas e removendo as colunas com dados desnecessarios
-    wb = load_workbook(rf'{criacao}{nome_empresa}.xlsx')
+    wb = load_workbook(f'{criacao}_{codigo}_{nome_empresa}_{data_inicio}_a_{data_fim}.xlsx')
     ws = wb.active
 
 # removendo as colunas com dados desnecessarios
@@ -85,15 +95,6 @@ for codigo in empresas_df['cod']:
     ws.delete_cols(3)
     ws.delete_cols(2)
 
-# dando nome para as colunas
-    ws['A1'] = 'EVENTO'
-    ws['B1'] = 'NOMEUNIDADE'
-    ws['C1'] = 'FUNCIONARIO'
-    ws['D1'] = 'NOMEFUNCIONARIO'
-    ws['E1'] = 'STATUSEVENTO'
-    ws['F1'] = 'NRRECIBO'
-    ws['G1'] = 'DATAINICIOCONDICAO'
-
 # alterando a largura das colunas
     ws.column_dimensions['A'].width = 54
     ws.column_dimensions['B'].width = 30
@@ -104,4 +105,4 @@ for codigo in empresas_df['cod']:
     ws.column_dimensions['G'].width = 21
 
 # salvando as planilhas alteradas
-    wb.save(f'{criacao}{nome_empresa}_{data_inicio}_a_{data_fim}.xlsx')
+    wb.save(f'{criacao}_{codigo}_{nome_empresa}_{data_inicio}_a_{data_fim}.xlsx')
